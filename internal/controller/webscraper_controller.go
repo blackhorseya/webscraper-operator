a3e75125381a9256c3218a546992c7b92a2621f7
Sean Zheng <blackhorseya@gmail.com>
2024-12-22 15:04:53 +0800

    chore: bump version and enhance webhook configurations
    - Update the default version from 0.0.1 to 0.0.2 in the Makefile
    - Add a path for CA injection in the kustomization YAML files
    - Update the image tag for the web scraper operator from 0.0.1 to 0.0.2 in the manager YAML
    - Add rules for managing cronjobs in the ClusterRole definition
    - Add environment variable to enable webhooks in the manager configuration
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- internal/controller/webscraper_controller.go
+++ internal/controller/webscraper_controller.go
@@ -41,6 +41,8 @@ type WebScraperReconciler struct {
 // +kubebuilder:rbac:groups=batch.seancheng.space,resources=webscrapers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=batch.seancheng.space,resources=webscrapers/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=batch.seancheng.space,resources=webscrapers/finalizers,verbs=update
+// +kubebuilder:rbac:groups=batch,resources=cronjobs,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=batch,resources=cronjobs/status,verbs=get;update;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
